c098d639bd64c5b6a3679916a862838b3db02368
VinogradovaOV <ka_2197@mail.ru>
2022-09-12 13:10:09 +0300

    Второе задание по курсу C#

---
+++ ProgramEasy_Lesson2/ProgramEasy_Lesson2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramEasy_Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Состав типа C#
+
+            //Демонстрация работы с методами и свойствами класса
+            Console.WriteLine("Присвоение переменной класса значения через метод:");
+            StudentClass studentClass = new StudentClass();
+            studentClass.Grow(15);
+            Console.WriteLine("Возраст студента  = " + studentClass.Age);
+            Console.ReadLine();
+
+            ////Демонстрация работы с конструкторм структуры
+            Console.WriteLine("Присвоение переменной структуры значения через конструктор:");
+            StudentStruct studentStruct = new StudentStruct(15);
+            
+            Console.WriteLine("Возраст студента  = " + studentStruct.Age);
+            Console.WriteLine("Масса студента ставится по умолчанию  = " + studentStruct.Mass);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ProgramEasy_Lesson2/ProgramEasy_Lesson2/StudentStruct.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramEasy_Lesson2
+{
+    struct StudentStruct
+    {
+        /// <summary>
+        /// Поле типа, приватное, используется только внутри типа.
+        /// Значения задаются с помощью методов типа или свойств типа.
+        /// </summary>
+        private int _age;
+
+        /// <summary>
+        /// Публичное свойство типа, задает и возвращает значение переменной _age
+        /// Можно добавить различные проверки внутрь свойства, например, что возраст не может быть меньше 0.
+        /// Сокращенная запись тривиального свойства public int Age{get; set;}
+        /// Так же можно отдельно задавать модификаторы доступа для get и set свойства
+        /// </summary>
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public int Mass { get; set; }
+
+        //Конструктор типа без параметров в структуре явно создавать нельзя!
+        //public StudentStruct()
+        //{
+        //    _age = 0;
+        //}
+
+        /// <summary>
+        /// Конструктор типа с параметром int
+        /// В конструкторе структуры надо обязательно инициализировать все переменные!!!
+        /// </summary>
+        /// <param name="age">Возраст студента</param>
+        public StudentStruct(int age)
+        {
+            this = new StudentStruct(); //Вызывем конструктор по умолчанию, он всем переменным присваивает 0(по умолчанию).
+            _age = age;
+        }
+    }
+}
