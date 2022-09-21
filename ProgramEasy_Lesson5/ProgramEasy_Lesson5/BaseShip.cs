95ffd5de3f76b2a8a76351e62892127f3b7c4671
VinogradovaOV <ka_2197@mail.ru>
2022-09-21 12:55:09 +0300

    Пятое задание по курсу C#

---
+++ ProgramEasy_Lesson5/ProgramEasy_Lesson5/BaseShip.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramEasy_Lesson5
+{
+    /// <summary>
+    /// Абстрактный класс кораблей, не имеет экземпляров, выступает в качестве родителя.
+    /// </summary>
+    public abstract class BaseShip
+    {
+        public abstract int Distatnse { get; set; } // Для абстрактных членов типа необходима реализация в наследнике
+        public abstract string Move(int distanse);
+
+        public void InfoCoordinat(int x, int y)
+        {
+            Console.WriteLine("Координаты корабля {0},{1}", x, y);
+        }
+    }
+
+    /// <summary>
+    /// Наследник абстрактного класса, запечатан - не может быть родителем.
+    /// </summary>
+    public sealed class TransportShip: BaseShip
+    {
+        static TransportShip()
+        {
+            Console.WriteLine("Статический конструктор класса, вызывается самым первым, один раз за всю программу.");
+
+        }
+
+        public static int size; //статическое поле одно для всех экземпляров типа
+
+        public override int Distatnse { get; set; }
+
+        public override string Move(int distanse)
+        {
+            return "Дистанция до корабля = " + distanse;
+        }
+
+        public string SizeShip()
+        {
+            return "" + size;
+        }
+    }
+
+    /// <summary>
+    /// Статический класс, не реализует наследование, не имеет экземпляров.
+    /// может содержать в себе только статические члены
+    /// </summary>
+    public static class BigShip
+    {
+        public static int size;
+
+        public static string SizeShip(int s)
+        {
+            return "Размер Большого корабля = " + s;
+        }
+    }
+}
---
+++ ProgramEasy_Lesson5/ProgramEasy_Lesson5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramEasy_Lesson5
+{
+    class Program
+    {
+        //Запечатанные, статические и австрактные классы. Урок по c# 5
+
+        static void Main(string[] args)
+        {
+            //
+            TransportShip transportShip = new TransportShip();
+            TransportShip transportShip1 = new TransportShip();
+            TransportShip.size = 50; // задание значения статическому полю
+
+            transportShip.Distatnse = 1000;//переопределенное свойство
+            Console.WriteLine(transportShip.Move(transportShip.Distatnse)); //переопределенный метод Move
+            transportShip.InfoCoordinat(10, 20);//метод абстрактного класса
+
+            //вывод значения статического поля для разных экземпляров
+            Console.WriteLine("Размер первого транспортного корабля = " + transportShip.SizeShip());
+            Console.WriteLine("Размер второго транспортного корабля = " + transportShip1.SizeShip());
+
+            //Использование полностью статического класса
+            BigShip.size = 100;
+            Console.WriteLine(BigShip.SizeShip(BigShip.size));
+            Console.ReadLine();
+        }
+    }
+}
